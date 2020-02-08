a94d39158809d5e6833366c174d97fb863da3e53
git <davidalonso79@gmail.com>
2020-02-08 15:56:49 +0100

    adding logging

--- src/main/java/es/vir2al/fwk/controllers/TestController.java
+++ src/main/java/es/vir2al/fwk/controllers/TestController.java
@@ -1,5 +1,7 @@
 package es.vir2al.fwk.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -11,9 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/test")
 public class TestController {
 
+	private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+			
 	@GetMapping()
-	@PreAuthorize("hasRole('TEST')")
+	@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<?> ping() {
+		
+		logger.info("TESTIIIIIING...");
 		
 		return new ResponseEntity<>("pong",HttpStatus.OK);
 		
