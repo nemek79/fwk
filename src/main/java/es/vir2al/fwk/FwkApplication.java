a9721934f8a3fa898d5c5b010d7c8240201181f8
DAVID ALONSO <d.alonso.sanchez@accenture.com>
2020-03-27 08:39:39 +0100

    scanning multiple packages

--- src/main/java/es/vir2al/fwk/FwkApplication.java
+++ src/main/java/es/vir2al/fwk/FwkApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan({"es.vir2al.fwk"})
 public class FwkApplication {
 
 	public static void main(String[] args) {
