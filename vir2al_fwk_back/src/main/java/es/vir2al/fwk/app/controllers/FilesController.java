851a2c467478c9bdf43d09a21dbcc077ecb1f5ff
David Alonso <davidalonso79@gmail.com>
2023-06-09 09:26:39 +0200

    feat: control de ficheros

---
+++ vir2al_fwk_back/src/main/java/es/vir2al/fwk/app/controllers/FilesController.java
@@ -0,0 +1,169 @@
+package es.vir2al.fwk.app.controllers;
+
+import java.io.File;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+
+import es.vir2al.fwk.fwk.domain.FileVO;
+import es.vir2al.fwk.fwk.domain.responses.BaseResponse;
+import es.vir2al.fwk.fwk.domain.responses.DataResponse;
+import es.vir2al.fwk.fwk.exceptions.BaseException;
+import es.vir2al.fwk.fwk.services.FilesService;
+import es.vir2al.fwk.fwk.utils.constants.ResponseConstants;
+
+@CrossOrigin(origins = { "http://localhost:4200", "*" })
+@RestController
+@RequestMapping("/api/files")
+public class FilesController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PaisesController.class);
+
+    @Autowired
+    FilesService filesService;
+
+
+    @GetMapping(value="/{filename:.+}",produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    @ResponseBody
+    public ResponseEntity<?> getFile(@PathVariable String filename) {
+      
+        Resource file;
+
+        try {
+            
+            file = filesService.load(filename);
+
+
+            return ResponseEntity.ok()
+             .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+
+        } catch (BaseException e) {
+
+            e.printStackTrace();
+            BaseResponse response = new BaseResponse();
+            response.setCode(ResponseConstants.UNEXPECTED_ERROR);
+            return new ResponseEntity<>(response,HttpStatus.OK);
+        }
+
+    }
+    
+    @PostMapping("/init")
+    public ResponseEntity<?> init() {
+
+        LOGGER.debug("INICIO init()");
+
+        BaseResponse response = new BaseResponse();
+
+        try {
+
+            this.filesService.init();
+        
+        } catch (BaseException e) {
+            
+            e.printStackTrace();
+            response.setCode(ResponseConstants.UNEXPECTED_ERROR);
+
+        }
+
+        LOGGER.debug("FINAL init()");
+
+        return new ResponseEntity<>(response,HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/deleteall")
+    public ResponseEntity<?> deleteAll() {
+
+        LOGGER.debug("INICIO deleteAll()");
+
+        BaseResponse response = new BaseResponse();
+
+        try {
+
+            this.filesService.deleteAll();
+        
+        } catch (BaseException e) {
+            
+            e.printStackTrace();
+            response.setCode(ResponseConstants.UNEXPECTED_ERROR);
+
+        }
+
+        LOGGER.debug("FINAL deleteAll()");
+
+        return new ResponseEntity<>(response,HttpStatus.OK);
+
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) {
+      
+        BaseResponse response = new BaseResponse();
+
+        try {
+            
+            this.filesService.save(file);
+  
+        } catch (Exception e) {
+
+            LOGGER.error(e.getMessage());
+			response.setCode(ResponseConstants.UNEXPECTED_ERROR);
+
+        }
+
+        LOGGER.debug("FINAL uploadFile()");
+
+        return new ResponseEntity<>(response,HttpStatus.OK);
+
+    }
+
+    @GetMapping("")
+    public ResponseEntity<?> getListFiles() {
+
+        List<FileVO> fileInfos;
+        DataResponse<List<FileVO>> response = new DataResponse<List<FileVO>>();
+
+        try {
+
+            fileInfos = filesService.loadAll().map(path -> {
+                
+                String filename = path.getFileName().toString();
+                String url = MvcUriComponentsBuilder
+                    .fromMethodName(FilesController.class, "getFile", path.getFileName().toString()).build().toString();
+    
+                return new FileVO(filename, url);
+            }).collect(Collectors.toList());
+
+            response.setData(fileInfos);
+
+        } catch (BaseException e) {
+
+            e.printStackTrace();
+            response.setCode(e.getCode());
+
+        }
+  
+        return new ResponseEntity<>(response,HttpStatus.OK);
+    }
+
+
+}
--- vir2al_fwk_back/src/main/java/es/vir2al/fwk/fwk/utils/constants/ResponseConstants.java
+++ vir2al_fwk_back/src/main/java/es/vir2al/fwk/fwk/utils/constants/ResponseConstants.java
@@ -3,10 +3,15 @@ package es.vir2al.fwk.fwk.utils.constants;
 public class ResponseConstants {
     
     public final static Integer     RESPONSE_OK             = 0;
+
+	public final static Integer		OBJECT_ALREADY_EXISTS	= 100;
+	public final static Integer		FILE_TOO_LONG           = 101;
+
 	public final static Integer		NOT_DEFINED				= -999;
 	public final static Integer     RESPONSE_KO             = -1;
 	public final static Integer		INPUT_DATA_ERROR		= -2;
 	public final static Integer		NODATA_FOUND_ERROR		= -3;
 	public final static Integer     UNEXPECTED_ERROR        = -100;
-
+	public final static Integer		IO_ERROR				= -101;
+	
 }
