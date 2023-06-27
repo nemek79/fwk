b97a3e5b9af20f895bab72fce9fc993ac12874ef
David Alonso <davidalonso79@gmail.com>
2023-06-28 00:08:20 +0200

    feat: configuracion de aspectos para log de inicio/fin de métodos

--- vir2al_fwk_back/src/main/java/es/vir2al/fwk/FwkApplication.java
+++ vir2al_fwk_back/src/main/java/es/vir2al/fwk/FwkApplication.java
@@ -2,8 +2,12 @@ package es.vir2al.fwk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableCaching
 public class FwkApplication {
 
 	public static void main(String[] args) {
